870e45635c82495c489f59d1614a09a0df14b890
Miguel García <ldv21c.mgarcia@uartesdigitales.edu.mx>
2022-03-24 09:41:42 -0600

    Added variables for score and damage

--- Scripts/BashBot.gd
+++ Scripts/BashBot.gd
@@ -6,11 +6,15 @@ export var dashImpulse = 25
 export var dampMultiplier = 5
 export var dashTime = 1.5
 export var dashDamp = 5
+export var damageResistance = .25
+export var damagePercentage = 0
 
 var cursorPosition = Vector3.ZERO
 var dashCharge = 1
 var dashPercentage = 0
+var score = 0
 
+onready var spawnPoint = get_transform()
 onready var mesh = $MeshInstance
 onready var cursor = $Cursor
 onready var arrow = $MeshInstance/Arrow
@@ -18,6 +22,8 @@ onready var camera = get_node("/root/Arena/GlobalCamera")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	contact_monitor = true
+	contacts_reported = 5
 	arrow.set_scale(Vector3(1,.25,1))
 	cursor.hide()
 
@@ -35,7 +41,6 @@ func run(_delta):
 	if Input.is_action_just_released("dash"):
 		linear_velocity = Vector3.ZERO
 		var direction = cursor.global_transform.origin - global_transform.origin
-		print(cursorPosition,", ",global_transform.origin,", keyboard")
 		direction = direction.normalized()
 		apply_central_impulse(direction*(dashPercentage*dashImpulse))
 		linear_damp = -1
@@ -74,3 +79,28 @@ func lookAtCursor(_delta):
 func _physics_process(_delta):
 	run(_delta)
 	lookAtCursor(_delta)
+
+func _on_BashBot_collision(collisionBashbot):
+	if collisionBashbot.name == "BashBotController":
+		print("keyboard first")
+		var accumulatedForce = 0
+		if collisionBashbot.linear_velocity.x < 0:
+			accumulatedForce += collisionBashbot.linear_velocity.x*-1
+		else:
+			accumulatedForce += collisionBashbot.linear_velocity.x
+		if collisionBashbot.linear_velocity.z < 0:
+			accumulatedForce += collisionBashbot.linear_velocity.z*-1
+		else:
+			accumulatedForce += collisionBashbot.linear_velocity.z
+		accumulatedForce /= 2
+		if accumulatedForce > 10:
+			if linear_velocity < collisionBashbot.linear_velocity:
+				damagePercentage += accumulatedForce * damageResistance
+				print(collisionBashbot," is ", damagePercentage, "% damaged")
+				apply_central_impulse(collisionBashbot.linear_velocity*-1*(damagePercentage/50))
+
+
+func _on_Area_body_exited(body):
+	print(body, " exited")
+	print(global_transform.origin)
+	set_transform(spawnPoint)
--- Scripts/BashBotController.gd
+++ Scripts/BashBotController.gd
@@ -6,6 +6,8 @@ export var dashImpulse = 25
 export var dampMultiplier = 5
 export var dashTime = 1.5
 export var dashDamp = 5
+export var damageResistance = .25
+export var damagePercentage = 0
 
 onready var mesh = $MeshInstance
 onready var cursor = $Cursor
@@ -15,9 +17,11 @@ onready var camera = get_node("/root/Arena/GlobalCamera")
 var dashCharge = 1
 var dashPercentage = 0
 var cursorPosition = Vector3.ZERO
+var score = 0
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	contact_monitor = true
+	contacts_reported = 5
 	arrow.set_scale(Vector3(1,.25,1))
 	cursor.hide()
 
@@ -71,3 +75,22 @@ func run(_delta):
 
 func _physics_process(_delta):
 	run(_delta)
+
+func _on_BashBot_collision(collisionBashbot):
+	if collisionBashbot.name == "BashBotKeyboard":
+		print("controller first")
+		var accumulatedForce = 0
+		if collisionBashbot.linear_velocity.x < 0:
+			accumulatedForce += collisionBashbot.linear_velocity.x*-1
+		else:
+			accumulatedForce += collisionBashbot.linear_velocity.x
+		if collisionBashbot.linear_velocity.z < 0:
+			accumulatedForce += collisionBashbot.linear_velocity.z*-1
+		else:
+			accumulatedForce += collisionBashbot.linear_velocity.z
+		accumulatedForce /= 2
+		if accumulatedForce > 10:
+			if linear_velocity < collisionBashbot.linear_velocity:
+				damagePercentage += accumulatedForce * damageResistance
+				print(collisionBashbot," is ", damagePercentage, "% damaged")
+				apply_central_impulse(collisionBashbot.linear_velocity*-1*(damagePercentage/50))
