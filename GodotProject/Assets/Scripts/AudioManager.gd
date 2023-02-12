9ceb8f52bcf810b64a2afb25e929eba0b7808708
Juan Bosco Sánchez Vargas Mora <JuanBoSVM@gmail.com>
2023-02-12 17:41:13 -0600

    Refactored and commented the audio manager

--- GodotProject/Assets/Scripts/AudioManager.gd
+++ GodotProject/Assets/Scripts/AudioManager.gd
@@ -1,45 +1,64 @@
-
 extends Node
-#var ASP = preload("res://Audio/AudioStreamPlayer.tscn")
 
-export(Array, AudioStream) var audios
+# Expose to the editor an array of AudioStream to place the game audios
+export(Array, AudioStream) var m_AudioDB: Array
 
-enum {ACTION_MISC_2, COLLECT_4, FLYING, LONG_SLIDE_DOWN, YOU_BEAST}
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Keys to all the audios
+enum { ACTION_MISC_2, COLLECT_4, FLYING, LONG_SLIDE_DOWN, YOU_BEAST }
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	makeSound(ACTION_MISC_2)
-	pass # Replace with function body.
+# Start playing the given sound without spacial positioning
+func makeSound(audioKey):
+	# Create the AudioStreamPlayer
+	var audioSP = AudioStreamPlayer.new()
 
-func makeSound(audio):
-	var aud = AudioStreamPlayer.new()
-	add_child(aud)
-	aud.set_stream(audios[audio])
-	aud.connect("finished",self,"onEnd")
-	aud.play()
+	# Add it to the scene
+	add_child(audioSP)
+
+	# Assign the given audio to the stream
+	audioSP.set_stream(m_AudioDB[audioKey])
+
+	# Connect the audio's finished signal to the Manager's onEnd method
+	audioSP.connect("finished", self, "onEnd")
+
+	# Play the audio
+	audioSP.play()
+
+
+# Start playing the given sound with a given spacial position
+func makeSoundAtPlace(audioKey, position):
+	# Create the AudioStreamPlayer
+	var audioSP = AudioStreamPlayer3D.new()
+
+	# Set the audio's position
+	audioSP.transform.position = position
 	
-func makeSoundAtPlace(audio, position):
-	var aud = AudioStreamPlayer3D.new()
-	aud.transform.position = position
-	add_child(aud)
-	aud.set_stream(audios[audio])
-	aud.connect("finished",self,"onEnd")
-	aud.play()
-	
-func _process(delta):
-	pass
-	#makeSound(ACTION_MISC_2)
-	
-#	pass
+	# Add it to the scene
+	add_child(audioSP)
 
+	# Assign the given audio to the stream
+	audioSP.set_stream(m_AudioDB[audioKey])
+
+	# Connect the audio's finished signal to the Manager's onEnd method
+	audioSP.connect("finished", self, "onEnd")
+
+	# Play the audio
+	audioSP.play()
+
+
+# Called when a given audio ends
 func onEnd():
+	# Get every currently created audio
 	var children = get_children()
+
+	# Loop through the audios
 	for child in children:
+
+		# Check if the audio isn't playing
 		if not child.is_playing():
+
+			# Remove the audio and innerhit all of its children
 			child.remove_and_skip()
+
+			# Destroy the audio node
 			child.queue_free()
-			print("deleted")
